a452322c35f36203a5437a262a6e0614d79cd61e
Guilherme Silva <guilherme.frr.silva@outlook.com>
2016-03-25 17:44:16 -0300

    Modelo Upload

---
+++ Modelo/Etiqueta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Etiqueta
+    {
+        //representa as etiquetas
+        public int id;
+        public string nome;
+        public float distH;
+        public float distV;
+        public float margemE;
+        public float margemT;
+        public float largura;
+        public float altura;
+        public byte numH;
+        public byte numV;
+    }
+}
---
+++ Modelo/ModeloEtq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ModeloEtq
+    {
+        public int id;
+        public Etiqueta etiqueta;
+        public string nome;
+        public string fonte_barcode;
+        public string fonte_legenda;
+        public byte tam_font_bc;
+        public byte tam_font_leg;
+        public string conteudo;
+        public string tipo_papel;
+        public int inicio;
+        public int intervalo;
+        public string formato;
+        public string prefixo;
+        public int quantidade;
+    }
+}
